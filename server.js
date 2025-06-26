139982718a1cd052e773110587028664affdd0e4
Seu Nome <tomialex443.com>
2025-06-26 15:48:04 -0300

    Corrige erro de sintaxe no server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const SECRET_KEY = 'ed7e892'; // sua chave fixa aqui
+const SECRET_KEY = 'sk_live_v2Q9kTcgVp92SAl7Q87UarEQeXXnqvg4mtCoUufvJI'; // sua chave fixa aqui
 
 app.post('/gerar-pix', async (req, res) => {
   const { value } = req.body;
@@ -32,7 +32,7 @@ app.post('/gerar-pix', async (req, res) => {
     customer: {
       name: "Gabriel Vieira",
       email: "gabriel@email.com",
-      phone: "21999999999", // Campo necessário
+      phone: "27996959606", // Campo necessário
       document: {
         number: "16695900701",
         type: "cpf"
